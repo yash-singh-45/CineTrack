394a6bf3a8f8b4e2820ed5944e16c544724e17f7
yash-singh-45 <yashrajput2022t@gmail.com>
2025-10-21 21:49:27 +0530

    Implement watchlist navigation button
    Added a button to navigate to the watchlist page.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,18 +8,51 @@ import TopRatedTV from './TopRatedTV';
 import TopRatedIndianTV from './TopRatedIndianTV';
 import IndianMovies from './IndianMovies';
 import Malayalam from './Malayalam';
-
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const [topRatedIndianTV, setTopRatedIndianTV] = useState([]);
     const apikey = import.meta.env.VITE_TMDB_API_KEY;
     const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"; // or w780, w300 depending on size
     const BASE_URL = "https://api.themoviedb.org/3";
-
+    const navigate = useNavigate();
+    function handleViewWatchList() {
+        navigate("/watchlist")
+    }
     return (
         <div className="bg-[#0F1115] text-white min-h-screen font-sans">
             <Navbar />
             <HeroSection />
+            <div className='md:px-10 px-2 my-3'>
+                <button
+                    onClick={handleViewWatchList}
+                    className='
+      px-4 md:px-6 py-2 text-sm md:text-2xl font-semibold text-white
+      bg-gradient-to-r from-blue-500 to-purple-600 
+      rounded-full 
+      shadow-lg hover:shadow-xl 
+      focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75
+      transform transition-all duration-300 
+      hover:scale-105 hover:-translate-y-1
+      active:scale-95
+      cursor-pointer
+      flex items-center space-x-2
+      content-center
+    '
+                >
+                    <span>View WatchList</span>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                    >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                    </svg>
+                </button>
+            </div>
             <TrendingMovies />
             <IndianMovies />
             <Malayalam />
