e5035c05cc82a549b6e99ee96f15c794ec125349
yash-singh-45 <yashrajput2022t@gmail.com>
2025-10-21 22:07:05 +0530

    Refactor MoviePage component for clarity and efficiency

--- src/Components/MoviePage.jsx
+++ src/Components/MoviePage.jsx
@@ -138,39 +138,25 @@ export default function MoviePage() {
               </div>
 
               <div className="flex items-center gap-3 mt-4">
-                {/* <div className="flex items-center gap-2">
-                  <svg className="w-6 h-6 text-yellow-400" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M12 .587l3.668 7.431 8.2 1.192-5.934 5.787 1.4 8.161L12 18.896 4.666 23.158l1.4-8.161L.132 9.21l8.2-1.192z" />
-                  </svg>
-                  <span className="text-lg font-bold">{movie.rating}</span>
-                </div> */}
-
-                <div className="flex gap-3 ml-10 md:text-sm lg:text-lg md:ml-0">
-                  <button onClick={handleWatchTrailer} className="px-4 py-2 bg-teal-500/95 rounded-full font-medium shadow-md hover:scale-[1.01] transition">
+                <div className="flex gap-3 text-sm md:text-sm lg:text-lg md:ml-5">
+                  <button onClick={handleWatchTrailer} className=" md:px-4 md:py-2 px-3 py-2 bg-teal-500/95 rounded-full font-medium shadow-md hover:scale-[1.01] transition">
                     ▶ Watch Trailer
                   </button>
+                
+                </div>
+              </div>
 
+              <div className="flex items-center gap-3 mt-4">
+                <div className="flex gap-3 text-sm md:text-sm lg:text-lg md:ml-5">
                   <button
                     onClick={inWatchlist?handleRemoveFromWatchlist :handleAddToWatchlist}
-                    className="px-4 py-2 bg-transparent border border-teal-400 text-teal-400 rounded-full font-medium shadow-md hover:bg-teal-500/20 transition"
+                    className="md:px-4 md:py-2 px-3 py-2 bg-transparent border border-teal-400 text-teal-400 rounded-full font-medium shadow-md hover:bg-teal-500/20 transition"
                   >
                     { inWatchlist? "❌ Remove from Watchlist" : "➕ Add to Watchlist"}
                   </button>
-
-
-                  {/* <button className="px-3 py-2 bg-transparent border border-gray-700 rounded-full text-gray-300">
-                    Remove from Watchlist
-                  </button> */}
-
-                  {/* <button className="w-10 h-10 rounded-full bg-transparent border border-gray-700 flex items-center justify-center text-gray-300">
-                    ❤
-                  </button>
-
-                  <button className="w-10 h-10 rounded-full bg-transparent border border-gray-700 flex items-center justify-center text-gray-300">
-                    ⇪
-                  </button> */}
                 </div>
               </div>
+
 
               {/* Controls */}
               <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4">
