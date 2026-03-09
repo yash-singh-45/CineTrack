050cb4e0f13653f0b58b5243c68a902c038c277a
yash-singh-45 <yashrajput2022t@gmail.com>
2026-03-10 03:46:10 +0530

    Refactor BlockCard component for improved functionality

--- src/Components/BlockCard.jsx
+++ src/Components/BlockCard.jsx
@@ -117,9 +117,9 @@ const BlockCard = ({ heroMovies }) => {
                         prevSlide();
                     }}
                     className="absolute left-0.5 md:left-2 top-1/2 -translate-y-1/2 z-10
-             h-12 w-8 md:h-14 md:w-10 rounded-full bg-black/40 backdrop-blur-md
+             h-12 w-8 md:h-20 md:w-12 rounded-full bg-black/40 backdrop-blur-md
              flex items-center justify-center text-white
-             hover:bg-black/70 transition cursor-pointer "
+             hover:bg-black/70 transition cursor-pointer opacity-0 hover:opacity-80"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 md:h-6 md:w-6 opacity-90" fill="none"
                         viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -133,9 +133,9 @@ const BlockCard = ({ heroMovies }) => {
                         nextSlide();
                     }}
                     className="absolute right-0.5 md:right-2 top-1/2 -translate-y-1/2 z-10
-             h-12 w-8 md:h-14 md:w-10 rounded-full bg-black/40 backdrop-blur-md
+             h-12 w-8 md:h-20 md:w-12 rounded-full bg-black/40 backdrop-blur-md
              flex items-center justify-center text-white
-             hover:bg-black/70 transition cursor-pointer "
+             hover:bg-black/70 transition cursor-pointer opacity-0 hover:opacity-80 "
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" className=" h-5 w-5 md:h-6 md:w-6 opacity-90" fill="none"
                         viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
